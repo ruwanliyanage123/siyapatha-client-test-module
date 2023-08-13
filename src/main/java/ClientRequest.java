504faec0f2d30179a1da0ba41a702dcacef4dd55
Ruwan <ruwanliyanagegalle1994@gmail.com>
2023-08-13 14:31:31 +0530

    Add multiple cliets to call the server

--- src/main/java/ClientRequest.java
+++ src/main/java/ClientRequest.java
@@ -8,21 +8,25 @@ public class ClientRequest {
     public ClientRequest(String name) {
         this.name = name;
     }
-    public void clientRequest(int number) {
+
+    public void clientRequest(int number, String token) {
         try {
             URL url = new URL("http://localhost:8080/book");
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("POST");
             connection.setRequestProperty("Content-Type", "application/json");
             connection.setRequestProperty("Accept", "application/json");
+            if (token != null) {
+                connection.setRequestProperty("Authorization", token);
+            }
             connection.setDoOutput(true);
-            String requestBody = "{\"isbn\": "+ number +", \"name\": \"The secret"+number+"\", \"author\": \""+name+"\", \"price\": 49.99}";
+            String requestBody = "{\"isbn\": " + number + ", \"name\": \"The secret" + number + "\", \"author\": \"" + name + "\", \"price\": 49.99}";
             try (OutputStream os = connection.getOutputStream()) {
                 byte[] input = requestBody.getBytes("utf-8");
                 os.write(input, 0, input.length);
             }
             int responseCode = connection.getResponseCode();
-            System.out.println("Response Code: " + responseCode);
+            System.out.println("Hi, " + name + " and " + number + " Response Code: " + responseCode);
             connection.disconnect();
         } catch (Exception e) {
             e.printStackTrace();
--- src/main/java/ClientRequestHandler.java
+++ src/main/java/ClientRequestHandler.java
@@ -1,12 +1,17 @@
 public class ClientRequestHandler {
     public static void main(String[] args) {
-       ClientRequest clientRequest = new ClientRequest("client1");
-       sentClientRequest(clientRequest, 0, 10);
+        ClientRequest client1 = new ClientRequest("client1");
+        ClientRequest client2 = new ClientRequest("client2");
+        ClientRequest client3 = new ClientRequest("client3");
+
+        sentClientRequest(client1, 100, 10, null);
+        sentClientRequest(client2, 200, 10, null);
+        sentClientRequest(client3, 300, 10, null);
     }
 
-    public static void sentClientRequest(ClientRequest clientRequest, int start, int count) {
+    public static void sentClientRequest(ClientRequest clientRequest, int start, int count, String token) {
         for (int i = start; i < start + count; i++) {
-            clientRequest.clientRequest(i);
+            clientRequest.clientRequest(i, token);
         }
     }
 }
